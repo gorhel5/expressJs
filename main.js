1ff4d13c90757e1ad97afea296ce56b90828e3d0
gorhel5 <Yaroslavlit@gmail.com>
2023-09-22 18:10:45 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({
+    extended: false
+}))
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+
+
+app.get('/user/:username', (req, res) => {
+    let data = {
+        username: req.params.username,
+        hobbies: [
+            'Football',
+            'Skate',
+            'Basketball'
+        ]
+    }
+
+    res.render('user', data)
+})
+
+app.post('/check-user', (req, res) => {
+    let username = req.body.username
+
+    if (username == '')
+        return res.redirect('/')
+    else 
+        return res.redirect('/user/' + username)
+})
+
+app.listen(3000, () => {
+    console.log('Server started: http://localhost:3000');
+})
